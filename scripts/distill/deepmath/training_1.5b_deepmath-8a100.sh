abc41bfc8ceca53895ca02cf8f751723b22a34de
Aochong Li <al2644@goyal-compute-01.cs.cornell.edu>
2025-05-28 13:04:42 -0400

    change the rollout temperature

--- scripts/distill/deepmath/training_1.5b_deepmath-8a100.sh
+++ scripts/distill/deepmath/training_1.5b_deepmath-8a100.sh
@@ -41,7 +41,7 @@ python3 -m verl.trainer.main_distill_data \
  actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
  actor_rollout_ref.rollout.tensor_model_parallel_size= 1\
  actor_rollout_ref.rollout.name=vllm \
- actor_rollout_ref.rollout.temperature=0.6 \
+ actor_rollout_ref.rollout.temperature=1.0\
  +actor_rollout_ref.rollout.val_temperature=0.6 \
  actor_rollout_ref.rollout.gpu_memory_utilization=0.75 \
  actor_rollout_ref.rollout.n=$ROLLOUT_N \
