904d29edb9626c6057905af9f92c97a16b54b135
Aochong Li <al2644@goyal-compute-01.cs.cornell.edu>
2025-05-29 21:31:10 -0400

    sft with self-distill data

--- sft/LLaMA-Factory/scripts/train/qwen25_math_deepmath_350k_8a6000s.sh
+++ sft/LLaMA-Factory/scripts/train/qwen25_math_deepmath_350k_8a6000s.sh
@@ -6,4 +6,4 @@ export WANDB_PROJECT="llamafactory-sft"
 FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/qwen25_math_full_self_distill.yaml
 
 ## Push checkpoints to huggingface
-python3 push_to_hf/experiments.py --project_name llamafactory-sft --run_name Qwen2.5-Math-1.5B-DeepMath-Hard-GRPO-1800-steps-SFT
+# python3 push_to_hf/experiments.py --project_name llamafactory-sft --run_name Qwen2.5-Math-1.5B-DeepMath-Hard-GRPO-1800-steps-SFT
