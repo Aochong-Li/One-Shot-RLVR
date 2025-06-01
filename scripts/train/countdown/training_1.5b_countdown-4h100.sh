b05259f0e02e1b3b0171364c676e489dda899618
Aochong Li <al2644@goyal-compute-01.cs.cornell.edu>
2025-06-01 11:02:48 -0400

    add empire ai launch script

---
+++ scripts/train/countdown/training_1.5b_countdown-4h100.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -x
+
+# CHECKPOINTS_DIR=... # TODO: change to your own path
+# Dataset Size: 490K
+# export HUGGINGFACE_HUB_TOKEN="YOUR_TOKEN_HERE"
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export VLLM_ATTENTION_BACKEND=XFORMERS
+export CHECKPOINTS_DIR="./outputs"
+export BASE_MODEL="Qwen/Qwen2.5-3B"
+
+N_GPUS=4
+ROLLOUT_N=8
+TENSOR_MODEL_PARALLEL_SIZE=1
+TOTAL_EPOCHS=1
+SAVE_STEPS=100
+EVAL_STEPS=50
+
+EXPERIMENT_NAME="Qwen2.5-3B-countdown-${TOTAL_EPOCHS}epochs-${ROLLOUT_N}rollouts"
+
+python3 -m verl.trainer.main_ppo \
+ algorithm.adv_estimator=grpo \
+ data.train_files=data/train/countdown/train.parquet \
+ data.val_files=data/train/countdown/test.parquet \
+ data.train_batch_size=256 \
+ data.val_batch_size=512 \
+ data.max_prompt_length=256 \
+ data.max_response_length=3840 \
+ reward_model.reward_manager='naive' \
+ actor_rollout_ref.model.path=$BASE_MODEL \
+ actor_rollout_ref.actor.optim.lr=1e-6 \
+ actor_rollout_ref.model.use_remove_padding=True \
+ actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+ actor_rollout_ref.actor.use_dynamic_bsz=True \
+ actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
+ actor_rollout_ref.actor.use_kl_loss=True \
+ actor_rollout_ref.actor.kl_loss_coef=0.001 \
+ actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+ actor_rollout_ref.model.enable_gradient_checkpointing=True \
+ actor_rollout_ref.actor.fsdp_config.param_offload=False \
+ +actor_rollout_ref.actor.fsdp_config.grad_offload=False \
+ actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+ actor_rollout_ref.rollout.tensor_model_parallel_size=$TENSOR_MODEL_PARALLEL_SIZE \
+ actor_rollout_ref.rollout.name=vllm \
+ actor_rollout_ref.rollout.temperature=1.0 \
+ +actor_rollout_ref.rollout.val_temperature=0.6 \
+ actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+ actor_rollout_ref.rollout.n=$ROLLOUT_N \
+ +actor_rollout_ref.rollout.n_val=1 \
+ actor_rollout_ref.ref.fsdp_config.param_offload=True \
+ algorithm.kl_ctrl.kl_coef=0.001 \
+ trainer.critic_warmup=0 \
+ trainer.logger=['console','wandb'] \
+ trainer.project_name='countdown'\
+ trainer.username=aochongoliverli \
+ trainer.experiment_name=$EXPERIMENT_NAME \
+ trainer.checkpoints_dir=$CHECKPOINTS_DIR \
+ trainer.resume_mode='disable' \
+ +trainer.val_before_train=True \
+ trainer.n_gpus_per_node=$N_GPUS \
+ trainer.nnodes=1 \
+ trainer.save_freq=$SAVE_STEPS \
+ trainer.test_freq=$EVAL_STEPS \
+ trainer.total_epochs=$TOTAL_EPOCHS \
+ trainer.push_to_hub=False 2>&1 | tee verl_demo.log
+
+ ## Push Saved Checkpoints to Huggingface
+ python3 push_to_hf/experiments.py --project_name countdown --run_name $EXPERIMENT_NAME
