2e0c0126c80f02f24ec044dff853f40e37c147fd
Aochong Li <al2644@g2-login-05.coecis.cornell.edu>
2025-06-20 18:50:14 -0400

    train with different temperature settings

--- scripts/train/deepmath/training_1.5b_deepmath-5to6-8a100.sh
+++ scripts/train/deepmath/training_1.5b_deepmath-5to6-8a100.sh
@@ -10,58 +10,61 @@ EVAL_STEPS=50 # 781 / 50 = 16 times
 ROLLOUT_N=8
 TENSOR_MODEL_PARALLEL_SIZE=1
 MAX_LENGTH=16384
-
-EXPERIMENT_NAME="R1-Distill-Qwen-1.5B-deepmath-level5-6-max-length-${MAX_LENGTH}-rollout-${ROLLOUT_N}"
+TEMPERATURES=(0.5 1.0 1.5)
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export CHECKPOINTS_DIR="./outputs"
 
-python3 -m verl.trainer.main_ppo \
- algorithm.adv_estimator=grpo \
- data.train_files=data/train/deepmath_level5-6/train.parquet \
- data.val_files=data/test/math500.parquet \
- data.train_batch_size=128 \
- data.val_batch_size=512 \
- data.max_prompt_length=256 \
- data.max_response_length=$MAX_LENGTH \
- reward_model.reward_manager='naive' \
- actor_rollout_ref.model.path=$BASE_MODEL \
- actor_rollout_ref.actor.optim.lr=1e-6 \
- actor_rollout_ref.model.use_remove_padding=True \
- actor_rollout_ref.actor.ppo_mini_batch_size=128 \
- actor_rollout_ref.actor.use_dynamic_bsz=True \
- actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
- actor_rollout_ref.actor.use_kl_loss=True \
- actor_rollout_ref.actor.kl_loss_coef=0.001 \
- actor_rollout_ref.actor.kl_loss_type=low_var_kl \
- actor_rollout_ref.model.enable_gradient_checkpointing=True \
- actor_rollout_ref.actor.fsdp_config.param_offload=False \
- +actor_rollout_ref.actor.fsdp_config.grad_offload=False \
- actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
- actor_rollout_ref.rollout.tensor_model_parallel_size=$TENSOR_MODEL_PARALLEL_SIZE \
- actor_rollout_ref.rollout.name=vllm \
- actor_rollout_ref.rollout.temperature=1.0 \
- +actor_rollout_ref.rollout.val_temperature=0.6 \
- actor_rollout_ref.rollout.gpu_memory_utilization=0.75 \
- actor_rollout_ref.rollout.n=$ROLLOUT_N \
- +actor_rollout_ref.rollout.n_val=1 \
- actor_rollout_ref.ref.fsdp_config.param_offload=True \
- algorithm.kl_ctrl.kl_coef=0.001 \
- trainer.critic_warmup=0 \
- trainer.logger=['console','wandb'] \
- trainer.project_name='verl_rlvr'\
- trainer.username='aochongoliverli' \
- trainer.experiment_name=$EXPERIMENT_NAME \
- trainer.checkpoints_dir=$CHECKPOINTS_DIR \
- trainer.resume_mode='disable' \
- +trainer.val_before_train=True \
- trainer.n_gpus_per_node=$N_GPUS \
- trainer.nnodes=1 \
- trainer.save_freq=$SAVE_STEPS \
- trainer.test_freq=$EVAL_STEPS \
- trainer.total_epochs=$TOTAL_EPOCHS \
- trainer.push_to_hub=False 2>&1 | tee $CHECKPOINTS_DIR/verl_rlvr.log
+for TEMPERATURE in ${TEMPERATURES}; do
+    EXPERIMENT_NAME="R1-Distill-Qwen-1.5B-deepmath-level5-6-max-length-${MAX_LENGTH}-rollout-${ROLLOUT_N}-temperature-${TEMPERATURE}"
 
- ## Push Saved Checkpoints to Huggingface
- python3 push_to_hf/experiments.py --project_name verl_rlvr --run_name $EXPERIMENT_NAME
+    python3 -m verl.trainer.main_ppo \
+            algorithm.adv_estimator=grpo \
+            data.train_files=data/train/deepmath_level5-6/train.parquet \
+            data.val_files=data/test/math500.parquet \
+            data.train_batch_size=128 \
+            data.val_batch_size=512 \
+            data.max_prompt_length=256 \
+            data.max_response_length=$MAX_LENGTH \
+            reward_model.reward_manager='naive' \
+            actor_rollout_ref.model.path=$BASE_MODEL \
+            actor_rollout_ref.actor.optim.lr=1e-6 \
+            actor_rollout_ref.model.use_remove_padding=True \
+            actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+            actor_rollout_ref.actor.use_dynamic_bsz=True \
+            actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
+            actor_rollout_ref.actor.use_kl_loss=True \
+            actor_rollout_ref.actor.kl_loss_coef=0.001 \
+            actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+            actor_rollout_ref.model.enable_gradient_checkpointing=True \
+            actor_rollout_ref.actor.fsdp_config.param_offload=False \
+            +actor_rollout_ref.actor.fsdp_config.grad_offload=False \
+            actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+            actor_rollout_ref.rollout.tensor_model_parallel_size=$TENSOR_MODEL_PARALLEL_SIZE \
+            actor_rollout_ref.rollout.name=vllm \
+            actor_rollout_ref.rollout.temperature=$TEMPERATURE \
+            +actor_rollout_ref.rollout.val_temperature=0.6 \
+            actor_rollout_ref.rollout.gpu_memory_utilization=0.75 \
+            actor_rollout_ref.rollout.n=$ROLLOUT_N \
+            +actor_rollout_ref.rollout.n_val=1 \
+            actor_rollout_ref.ref.fsdp_config.param_offload=True \
+            algorithm.kl_ctrl.kl_coef=0.001 \
+            trainer.critic_warmup=0 \
+            trainer.logger=['console','wandb'] \
+            trainer.project_name='verl_rlvr'\
+            trainer.username='aochongoliverli' \
+            trainer.experiment_name=$EXPERIMENT_NAME \
+            trainer.checkpoints_dir=$CHECKPOINTS_DIR \
+            trainer.resume_mode='disable' \
+            +trainer.val_before_train=True \
+            trainer.n_gpus_per_node=$N_GPUS \
+            trainer.nnodes=1 \
+            trainer.save_freq=$SAVE_STEPS \
+            trainer.test_freq=$EVAL_STEPS \
+            trainer.total_epochs=$TOTAL_EPOCHS \
+            trainer.push_to_hub=False 2>&1 | tee $CHECKPOINTS_DIR/verl_rlvr.log
+
+    ## Push Saved Checkpoints to Huggingface
+    python3 push_to_hf/experiments.py --project_name verl_rlvr --run_name $EXPERIMENT_NAME
+done
