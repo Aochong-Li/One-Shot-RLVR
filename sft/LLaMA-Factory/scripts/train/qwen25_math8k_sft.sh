5c0aa819dd0c504f179b13de5c275898dc6dac60
Aochong Li <al2644@alpha1.eth.cluster>
2025-09-02 16:03:33 -0400

    sft for 1.5B models

--- sft/LLaMA-Factory/scripts/train/qwen25_math8k_sft.sh
+++ sft/LLaMA-Factory/scripts/train/qwen25_math8k_sft.sh
@@ -1,10 +1,16 @@
 set -x
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 export WANDB_PROJECT="math8k"
 
+# 3B Full Fine-Tuning
 # FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_math8k/qwen25_3b_math8k_qwq.yaml
 # FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_math8k/qwen25_3b_math8k_am.yaml
 # FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_math8k/qwen25_3b_math8k_qwen3.yaml
 # FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_math8k/qwen25_3b_math8k_qwq_limo.yaml
-FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_limo/qwen25_3b_math8k_limo.yaml
+# FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_limo/qwen25_3b_math8k_limo.yaml
+
+# 1.5B Full Fine-Tuning
+FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_math8k/qwen25_1.5b_math8k_qwq.yaml
+FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_math8k/qwen25_1.5b_math8k_am.yaml
+FORCE_TORCHRUN=1 llamafactory-cli train examples/train_distill_math8k/qwen25_1.5b_math8k_qwen3.yaml
